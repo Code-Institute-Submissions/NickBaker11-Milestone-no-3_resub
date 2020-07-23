78b16827e663179a84bd89adba1b98d75446064a
Nick <nickbaker11@gmail.com>
2020-07-23 08:55:11 +0000

    Templates folder added, setup connection to mongodb- needs completing

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
 
+app.config['MONGO_DBNAME'] = 'dino-index'
+app.config['MONGO_URI'] = 'mongo "mongodb+srv://cluster0.pgi7y.mongodb.net/dino-index" --username dinoguy'
+
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello world!'
+@app.route('/get_dinoInfo')
+def get_dinoInfo():
+    return render_template('info.html', dinoInfo=mongo.db.dinoInfo.find())
 
 
 if __name__ == '__main__':
