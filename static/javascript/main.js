a7fcd234a5673156bce6c76a099e997cad8cbca0
Nick <nickbaker11@gmail.com>
2020-08-17 19:08:15 +0000

    Database filter section frontend

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -6,3 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
   });
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
